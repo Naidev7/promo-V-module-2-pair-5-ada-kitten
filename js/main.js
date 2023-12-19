72b2654848bbf9b954fab8aa25635883803c6dfd
crodriguez1992 <crpzgz92@gmail.com>
2023-12-19 11:14:49 +0100

    create variables and add li to ul using js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,72 @@
 'use strict';
 const catList = document.querySelector('.js-list');
+
+/*CAT 1*/
+
+const kittenOneImage = 'https://dev.adalab.es/gato-siames.webp';
+const kittenOneName = 'Anastacio';
+const kittenOneDesc = ' Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
+const kittenOneRace = 'Siamés';
+
+const kittenOne = `<li class="card">
+<article>
+  <img
+    class="card_img"
+    src= ${kittenOneImage}
+    alt="gatito"
+  />
+  <h3 class="card_title"> ${kittenOneName}</h3>
+  <h4 class="card_race">${kittenOneRace}</h4>
+  <p class="card_description">
+            ${kittenOneDesc}
+   </p>
+</article>
+</li>`;
+
+console.log(kittenOne);
+
+
+/*CAT 2*/
+const kittenTwoImage = 'https://dev.adalab.es/sphynx-gato.webp';
+const kittenTwoName = 'Fiona';
+const kittenTwoDesc = 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño…\ hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.';
+const kittenTwoRace = 'Sphynx';
+
+const kittenTwo = `<li class="card">
+<img
+  class="card_img"
+  src=${kittenTwoImage}
+  alt="sphynx-cat"
+/>
+<h3 class="card_title">${kittenTwoName}</h3>
+<h4 class="card_race">${kittenTwoRace}</h4>
+<p class="card_description">
+  ${kittenTwoDesc}
+</p>
+</li>`
+
+
+/*CAT 3*/
+
+const kittenThreeImage = 'https://dev.adalab.es/maine-coon-cat.webp';
+const kittenThreeName = 'Cielo';
+const kittenThreeDesc = 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.';
+const kittenThreeRace = 'Maine Coon';
+
+const kittenThree = `<li class="card">
+<img
+  class="card_img"
+  src=${kittenThreeImage}
+  alt="maine-coon-cat"
+/>
+<h3 class="card_title">${kittenThreeName}</h3>
+<h4 class="card_race">${kittenThreeRace}</h4>
+<p class="card_description">
+${kittenThreeDesc}
+</p>
+</li>`
+
+
+/* LLAMAR A LOS li AL ul del html */
+
+catList.innerHTML = kittenOne + kittenTwo + kittenThree;
